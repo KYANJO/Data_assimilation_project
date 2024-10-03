cbe5d9fefdbbabdc38a3654b6667b38a9ff32c2a
Kyanjo <brian.kyanjo@aims.ac.rw>
2024-10-03 08:47:51 -0400

    new file for storing utility functions

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -9,5 +9,5 @@ def safe_chdir(main_directory,target_directory):
     # Check if the target path starts with the main directory path
     if target_path.startswith(main_directory):
         os.chdir(target_directory)
-    else:
-        print(f"Error: Attempted to leave the main directory '{main_directory}'.")
+    # else:
+    #     print(f"Error: Attempted to leave the main directory '{main_directory}'.")
