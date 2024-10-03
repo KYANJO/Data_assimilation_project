edf5dcbdfd2df22b5fb564dce805a4f8b5423c51
Kyanjo <brian.kyanjo@aims.ac.rw>
2024-10-03 08:46:22 -0400

    added new setup file

---
+++ src/utils/tools.py
@@ -0,0 +1,13 @@
+
+import os
+
+# Function to safely change directory
+def safe_chdir(main_directory,target_directory):
+    # Get the absolute path of the target directory
+    target_path = os.path.abspath(target_directory)
+
+    # Check if the target path starts with the main directory path
+    if target_path.startswith(main_directory):
+        os.chdir(target_directory)
+    else:
+        print(f"Error: Attempted to leave the main directory '{main_directory}'.")
