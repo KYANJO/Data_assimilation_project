1d20912144431a357087f74ea8717c7398bd0533
Kyanjo <brian.kyanjo@aims.ac.rw>
2024-10-04 15:02:41 -0400

    added dependency control file

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -1,5 +1,13 @@
+# ==============================================================================
+# @des: This file contains helper functions that are used in the main script.
+# @date: 2024-10-4
+# @author: Brian Kyanjo
+# ==============================================================================
 
 import os
+import sys
+import subprocess
+
 
 # Function to safely change directory
 def safe_chdir(main_directory,target_directory):
@@ -11,3 +19,16 @@ def safe_chdir(main_directory,target_directory):
         os.chdir(target_directory)
     # else:
     #     print(f"Error: Attempted to leave the main directory '{main_directory}'.")
+
+
+def install_requirements():
+    """
+    Install dependencies listed in the requirements.txt file if not already installed.
+    """
+    try:
+        # Run the command to install the requirements from requirements.txt
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "../requirements.txt"])
+    except subprocess.CalledProcessError as e:
+        # Print the error and raise a more meaningful exception
+        print(f"Error occurred while installing dependencies: {e}")
+        raise RuntimeError("Failed to install dependencies from requirements.txt. Please check the file and try again.")
